7c0308d883453a47c511028ae95517bef1653d7c
Hemanth Reddy <kollahemanthreddy@gmail.com>
2026-08-13 08:22:04 +0530

    Add Mongoose schemas for User, Post, and Comment models

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import User from "./models/user.js";
 dotenv.config();
 const app = express();
 app.use(cors());
---
+++ server/models/post.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    category: {
+      type: String,
+      default: "General",
+    },
+    coverImage: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
